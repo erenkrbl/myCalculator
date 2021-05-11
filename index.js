f9f2ff5fc3f7bac837fdfca8fb169c865b6108d2
Joseph Abraham <ekurubal24@gmail.com>
2021-05-11 13:51:28 +0300

    Added calculator class

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+class Calculator{
+    constructor(prevOperad, currentOprerand){
+        this.prevOperad = prevOperad;
+        this.currentOprerand = currentOprerand;
+    }
+    clear() {
+
+    }
+
+    delete() {
+
+    }
+
+    appendNumber() {
+
+    }
+
+    chooseOperation() {
+
+    }
+
+    compute() {
+
+    }
+
+    getDisplayNumber() {
+
+    }
+
+    updateDisplay() {
+
+    }
+
+}
+
 const numberButtons = document.querySelectorAll('[data-number]');
 const oprerationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelector('[data-equals]');
@@ -7,6 +42,8 @@ const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
 //console.log(allClearButton);
+
+const calulator = new Calculator(previousOperandTextElement, currentOperandTextElement);
 
 numberButtons.forEach((button) => {
     button.addEventListener("click", function(){
